24d0ebbacda47e3d17437b0ce2b6981cb51f2da4
Abhi Gargeshwari <gargesha@oregonstate.edu>
2026-02-07 19:39:08 -0800

    completed phase 1

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.get("/")
-def home():
-    return "Milestone #1 is running!"
+def index():
+    return redirect(url_for("entry"))
+
+@app.get("/entry")
+def entry():
+    return render_template("entry.html", status=None)
+
+@app.get("/history")
+def history():
+    return render_template("history.html", workouts=[])
+
+@app.get("/progress")
+def progress():
+    return render_template("progress.html", stats=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
